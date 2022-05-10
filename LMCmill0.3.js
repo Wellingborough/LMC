227d6ac5340d8e4a443aacde3a18b28f56adb29e
Mark Gamble <58432939+Wellingborough@users.noreply.github.com>
2022-05-10 20:55:30 +0100

    Update LMCmill0.3.js

--- LMCmill0.3.js
+++ LMCmill0.3.js
@@ -393,7 +393,10 @@ function filltable() {
   
   if (is_ios && is_safari){
     percent = 0.80;
-  }  
+    alert("Crapple");
+  }  else {
+    alert("Allegedly not Crapple");
+  }
   
   table1 = new Tabulator("#code-table", {
     maxHeight:'${height*percent}px', // set height of table (in CSS or here)
