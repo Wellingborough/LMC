6f410e399e45202e46e14a0402f2ddac61dd4d03
Mark Gamble <58432939+Wellingborough@users.noreply.github.com>
2024-05-29 16:46:08 +0100

    Adding Auth0

--- CAIEmill.js
+++ CAIEmill.js
@@ -4041,3 +4041,30 @@ function clearCode(){
 
   table1.setData(codetabledata);
 }
+
+//
+// Auth0 code
+//
+let auth0Client = null;
+const fetchAuthConfig = () => fetch("/auth_config.json");
+const configureClient = async () => {
+  const response = await fetchAuthConfig();
+  const config = await response.json();
+
+  auth0Client = await auth0.createAuth0Client({
+    domain: config.domain,
+    clientId: config.clientId
+  });
+};
+
+window.onload = async () => {
+  await configureClient();
+  updateUI();
+};
+
+const updateUI = async () => {
+  const isAuthenticated = await auth0Client.isAuthenticated();
+
+  document.getElementById("btn-logout").disabled = !isAuthenticated;
+  document.getElementById("btn-login").disabled = isAuthenticated;
+};
